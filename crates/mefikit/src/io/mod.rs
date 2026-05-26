8a0d40d7577c19ec94853daaa828002a59f41601
Floriane TUERNAL SABOTINOV <ftuertinov@proton.me>
2026-05-26 15:37:20 +0200

    revert cgns

--- crates/mefikit/src/io/mod.rs
+++ crates/mefikit/src/io/mod.rs
@@ -4,7 +4,6 @@ use std::path::Path;
 mod serde_io;
 pub mod vtk_io;
 mod hdfvtk_io;
-mod cgns_io;
 // mod med; // for later
 
 pub fn read(path: &Path) -> Result<UMesh, Box<dyn std::error::Error>> {
@@ -19,7 +18,6 @@ pub fn read(path: &Path) -> Result<UMesh, Box<dyn std::error::Error>> {
         "yaml" | "yml" => serde_io::read_yaml(path),
         "vtk" | "vtu" => vtk_io::read(path),
         "vtkhdf" | "h5" | "hdf5" => hdfvtk_io::read(path),
-        "cgns" => todo!(),
         _ => Err(format!("Unsupported file extension: {path:?}").into()),
     }
 }
@@ -36,7 +34,6 @@ pub fn write(path: &Path, mesh: UMeshView) -> Result<(), Box<dyn std::error::Err
         "yaml" | "yml" => serde_io::write_yaml(path, mesh),
         "vtk" | "vtu" => vtk_io::write(path, mesh),
         "vtkhdf" | "h5" | "hdf5" => hdfvtk_io::write(path, mesh),
-        "cgns" => cgns_io::write(path, &mesh),
         _ => Err(format!("Unsupported file extension: {path:?}").into()),
     }
 }
